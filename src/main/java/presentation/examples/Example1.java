db1e10a940b0740ed9743e88e07a520a23225825
Serhii Kartashov <kartashovsearhiy@gmail.com>
2016-03-04 16:54:24 +0200

    dependencies: akka-stream, groovy-all, rxjava, commons-collections4, guava

--- src/main/java/presentation/examples/Example1.java
+++ src/main/java/presentation/examples/Example1.java
@@ -1,7 +1,25 @@
 package presentation.examples;
 
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.javadsl.Source;
+
 /**
  * Created by Serhii K. on 3/1/2016.
  */
 public class Example1 {
+    final Source<Integer, NotUsed> source = Source.range(1, 100);
+
+    final ActorSystem system = ActorSystem.create("QuickStart");
+    final Materializer materializer = ActorMaterializer.create(system);
+
+    public static void main(String[] args) {
+        new Example1().foo();
+    }
+
+    private void foo() {
+        source.runForeach(i -> System.out.println(i), materializer);
+    }
 }
